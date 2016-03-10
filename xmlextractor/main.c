659aac6a7bb5d81d3d11b9025c7269f4a096f8c3
Frédéric Dalleau <fred@dalleau.re>
2016-03-10 07:48:53 +0100

    Initial Commit

---
+++ xmlextractor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xmlextractor
+//
+//  Created by fredo on 10/03/2016.
+//  Copyright © 2016 dalleau.re. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
